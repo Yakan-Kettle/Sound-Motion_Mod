ab91271486c593e3481ee75adf2b12f98c437d59
Yakan-Kettle <habaright@yahoo.co.jp>
2016-10-16 17:26:57 +0900

    おためし
    まるで使い方わかってないマン

---
+++ cpp_test/test_line1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+static const int MaxLength = 16;
+
+int main() {
+
+  char *str = new char[MaxLength];//メモリ確保
+  /*なんか操作する*/
+  /*delete 忘れ */
+  return 0;
+}
